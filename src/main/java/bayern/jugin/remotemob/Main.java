79ac39f39c41c62926bf8e42edf7e412670d1e7d
Florian Heubeck <heubeck@mediamarktsaturn.com>
2021-05-11 13:54:50 +0200

    RemoteMob Projektsetup

---
+++ src/main/java/bayern/jugin/remotemob/Main.java
@@ -0,0 +1,9 @@
+package bayern.jugin.remotemob;
+
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+    
+}
---
+++ src/test/java/bayern/jugin/remotemob/MainTest.java
@@ -0,0 +1,13 @@
+package bayern.jugin.remotemob;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMainInstanziation() {
+        var main = new Main();
+        assert main != null;
+    }
+
+}
